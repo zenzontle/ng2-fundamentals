5f8d02dab1ab5bc041f78d7ca73904cbf5bfe5d1
Jorge Hernandez <zenzontle@gmail.com>
2017-02-27 16:54:07 -0700

    Add Exploring the New Template Syntax module

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -4,10 +4,18 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
     selector: 'event-thumbnail',
     templateUrl: 'app/events/event-thumbnail.component.html',
     styles: [`
+        .thumbnail { min-height: 210px; }
         .pad-left { margin-left: 10px; }
         .well div {color: #bbb}
     `]
 })
 export class EventThumbnailComponent {
     @Input() event:any;
+
+    getStartTimeStyle():any {
+        if (this.event && this.event.time === '8:00 am') {
+            return {color: '#003300', 'font-weight': 'bold'};
+        }
+        return {};
+    }
 }
